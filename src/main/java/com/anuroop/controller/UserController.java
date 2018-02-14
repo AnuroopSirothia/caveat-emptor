da61a55c15565cb358e4423dd17eb0e6d8417478
Anuroop Sirothia <anuroop.sirothia@in.unisys.com>
2018-02-14 12:09:44 +0530

    User getting passed from REST WS
    User is getting passed from Postman as a JSON and getting converted to
    User object. Persisting the object is pending.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	deleted:    src/main/java/com/anuroop/controller/GreetingController.java
    	modified:   src/main/java/com/anuroop/controller/HelloController.java
    	new file:   src/main/java/com/anuroop/controller/UserController.java
    	modified:   src/main/java/model/User.java

--- src/main/java/com/anuroop/controller/UserController.java
+++ src/main/java/com/anuroop/controller/UserController.java
@@ -1,29 +1,18 @@
 package com.anuroop.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.User;
 
 @RestController
-@RequestMapping("/hi")
-public class HelloController {
-	private static final String template = "Hello %s!";
-	private final AtomicLong counter = new AtomicLong();
+public class UserController {
 	
-	@RequestMapping("/hello")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Duniya") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
-	
-	@RequestMapping(value = "/usertest", method = RequestMethod.POST)
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	public ResponseEntity<String> save(@RequestBody User user) {
 		System.out.println("DEBUG: " + user);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -15,4 +15,17 @@ public class User {
 	
 	@Column
 	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + "]";
+	}
 }
